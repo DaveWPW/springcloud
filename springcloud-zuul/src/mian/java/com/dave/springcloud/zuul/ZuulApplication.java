80cd48160be4dd54aa6a20f351a2ea99c9be2677
Dave <hellopeiwen@163.com>
2019-11-14 22:50:40 +0800

    添加 Zuul (API网关)

---
+++ springcloud-zuul/src/mian/java/com/dave/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.dave.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author: Dave
+ * @Date: 2019/11/14 21:17
+ * @Description: TODO
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
