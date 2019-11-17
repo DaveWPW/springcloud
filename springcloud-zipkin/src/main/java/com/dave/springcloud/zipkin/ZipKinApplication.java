7c421e8cfac4c121c39fb37c9bd09ecf70a16175
Dave <hellopeiwen@163.com>
2019-11-18 01:05:35 +0800

    ZipKin服务追踪（所有项目添加zipkin依赖，添加配置）

---
+++ springcloud-zipkin/src/main/java/com/dave/springcloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,20 @@
+package com.dave.springcloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @Author: Dave
+ * @Date: 2019/11/18 0:12
+ * @Description: TODO
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
